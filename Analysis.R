6ddb32ee3c860f230f97db54261dcbfd139d4c98
kiristern <kiri.a.stern@gmail.com>
2020-03-23 11:42:58 -0400

    remove asv 17 because not part of top 17

--- Analysis.R
+++ Analysis.R
@@ -70,6 +70,7 @@ top17 <- head(asv_tot, 17)
 top17 <- read.csv("top17.csv")
 
 asv_tax <- (asv)[,1:18]
+asv_tax <- asv_tax[,-17]
 nrow(asv_tax)
 #relative abundance matrix
 asv_rel_abun <- decostand(asv_tax, method="total")
