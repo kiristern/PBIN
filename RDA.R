99c1eb546798b15ddee799623496f274213ffb91
kiristern <kiri.a.stern@gmail.com>
2020-03-24 15:15:01 -0400

    rename cols

--- RDA.R
+++ RDA.R
@@ -5,7 +5,7 @@ library(mvpart)
 library(MVPARTwrap)
 library(rdaTest)
 library(labdsv)
-library(plyr)
+library(dplyr)
 library(MASS)
 library(tidyverse)
 setwd("~/Documents/GitHub/PBIN")
@@ -18,24 +18,33 @@ enviro_var[1] <- as.Date(enviro_var$Date)
 
 enviro_var <- enviro_var %>%
   rename(
-    Cumul_precip = Cumulative_precipitation_t1_t7_mm,
-    Avg_temp = Mean_temperature_t0_t7
+    Cumul_precip = "Cumulative_precipitation_t1_t7_mm",
+    Avg_temp = "Mean_temperature_t0_t7",
+    Tot_P = "Total_Phosphorus_ug",
+    Tot_N = "Total_Nitrogen_mg"
   )
 # when tidyverse decides not to load and don't want to restart R:
 # enviro_var <- rename(enviro_var, c("Cumulative_precipitation_t1_t7_mm"="Cumul_precip", "Mean_temperature_t0_t7" = "Avg_temp"))
 
 #Remove rows with too many NAs
 summary(enviro_var)
-env_var <- enviro_var %>% select(1:6, 12:13)
-env_var <- env_var[complete.cases(env_var), ]
-env_var %>% dplyr::glimpse()
-summary(env_var)
+#env_var <- enviro_var %>% select(1:6, 12:13)
+
+#remove date col
+env_keep <- enviro_var[,-1]
+
+#check how many rows there are without any NAs
+complete.cases(env_keep)
+
+complete_env_keep <- env_keep[complete.cases(env_keep), ]
+complete_env_keep %>% dplyr::glimpse()
+summary(complete_env_keep)
 
 abundance <- t(data.matrix(abund_table))
 
 #look at the species' distribution frequencies
 (viral_ab <- table(unlist(abundance)))
-barplot(viral_ab, las=1, xlab = "Abundance class", ylab="Frequency")
+# barplot(viral_ab, las=1, xlab = "Abundance class", ylab="Frequency")
 
 #see how many absences
 sum(abundance==0)
@@ -43,51 +52,54 @@ sum(abundance==0)
 #look at the proportion of zeros in community data
 sum(abundance==0)/(nrow(abundance)*ncol(abundance))
 
-#
+#comparing removed rows with abundance samples
 abund_name <- row.names(abundance)
-env_row_name <- row.names(env_var)
+env_row_name <- row.names(complete_env_keep)
 
 #check which rows are not the same
 abund_name %in% env_row_name
 #which rows are the same
 #intersect(abund_name, env_row_name)
+
 #specific samples that are not the same
-row_remove <- setdiff(abund_name, env_row_name)
+(row_remove <- setdiff(abund_name, env_row_name))
 #count how many are different
-#length(setdiff(abund_name, env_row_name))
+length(setdiff(abund_name, env_row_name))
 
 #remove rows (samples) that aren't in env_var from abundance
-abundance <- abundance[!(row.names(abundance) %in% row_remove), ]
+abundance_removed <- abundance[!(row.names(abundance) %in% row_remove), ]
 
 #transform repsonse variables
 #The transformation consists of expressing each fish density as a proportion of the sum of all densities 
 #in the analytical unit and taking the square root of the resulting value (Legendre and Gallagher 2001).
 #The square-root portion of the transformation decreases the importance of the most abundant species.
-abun_norm <- decostand(abundance, "hellinger")
+abun_norm <- decostand(abundance_removed, "hellinger")
 
-dim(abun_norm)
-str(abun_norm)
-head(abun_norm)
-summary(abun_norm)
+# dim(abun_norm)
+# str(abun_norm)
+# head(abun_norm)
+# summary(abun_norm)
 
 
 ##### Environmental data #######
-names(env_var)
-dim(env_var)
-str(env_var)
-head(env_var)
-summary(env_var)
-pairs(env_var, main="Bivariate Plots of the Environmental Data" ) 
+names(complete_env_keep)
+dim(complete_env_keep)
+str(complete_env_keep)
+head(complete_env_keep)
+summary(complete_env_keep)
+#pairs(env_keep, main="Bivariate Plots of the Environmental Data" ) 
 
 # #Standardize the environmental data (11 variables) using the function decostand() in vegan.
-# env_std <- decostand(env_var, method = "standardize")
+# env_std <- decostand(env_keep, method = "standardize")
 # apply(env_std, 2, mean) #centered data (mean~0)
 # apply(env_std, 2, sd) #scaled data (std dev=1)
 
 ######## RDA #######
 
 ?rda
-rda_spe <- rda(abun_norm~., data = env_var)
+rda_spe <- rda(abun_norm ~ Months + Years + Site + Period + bloom2 + Total_Phosphorus_ug + 
+                 Total_Nitrogen_mg + Dissolved_P + Dissolved_N + Cumul_precip + Avg_temp, 
+               data = complete_env_keep)
 
 #extract results
 summary(rda_spe, display = NULL)
@@ -95,7 +107,7 @@ summary(rda_spe, display = NULL)
 
 #select the significant explanatory variables
 ?ordiR2step
-ordiR2step(rda(abun_norm~1, data=env_var), scope= formula(rda_spe), direction= "forward", R2scope=TRUE, pstep=1000)
+ordiR2step(rda(abun_norm~1, data=complete_env_keep), scope= formula(rda_spe), direction= "forward", R2scope=TRUE, pstep=1000)
   #the proportion of variation explained by the constraining variables being 0.3542
 
 #retain significant variables only
@@ -130,23 +142,23 @@ plot(rda_spe_signif, scaling=2, main="Triplot RDA (scaling 2)")
   #distance among objects approximate their Euclidean distances
   #used to interpret distances among objects
 windows()
-plot(rda_spe_signif, scaling=1, main="Triplot RDA - scaling 1", type="none", xlab=c("RDA1"), ylab=c("RDA3"), xlim=c(-1,1), ylim=c(-1,1))
-points(scores(rda_spe_signif, display="sites", choices=c(1,3), scaling=1),
+plot(rda_spe_signif, scaling=1, main="Triplot RDA - scaling 1", type="none", xlab=c("RDA1"), ylab=c("RDA2"), xlim=c(-1,1), ylim=c(-1,1))
+points(scores(rda_spe_signif, display="sites", choices=c(1,2), scaling=1),
        pch=21, col="black", bg="steelblue", cex=1.2)
 arrows(0,0,
        scores(rda_spe_signif, display="species", choices=c(1), scaling=1),
-       scores(rda_spe_signif, display="species", choices=c(3), scaling=1),
+       scores(rda_spe_signif, display="species", choices=c(2), scaling=1),
        col="black",length=0)
 text(scores(rda_spe_signif, display="species", choices=c(1), scaling=1),
-     scores(rda_spe_signif, display="species", choices=c(3), scaling=1),
+     scores(rda_spe_signif, display="species", choices=c(2), scaling=1),
      labels=rownames(scores(rda_spe_signif, display="species", scaling=1)),
      col="black", cex=0.8)    
 arrows(0,0,
        scores(rda_spe_signif, display="bp", choices=c(1), scaling=1),
-       scores(rda_spe_signif, display="bp", choices=c(3), scaling=1),
+       scores(rda_spe_signif, display="bp", choices=c(2), scaling=1),
        col="red")
 text(scores(rda_spe_signif, display="bp", choices=c(1), scaling=1)+0.05,
-     scores(rda_spe_signif, display="bp", choices=c(3), scaling=1)+0.05,
+     scores(rda_spe_signif, display="bp", choices=c(2), scaling=1)+0.05,
      labels=rownames(scores(rda_spe_signif, display="bp", choices=c(2), scaling=1)),
      col="red", cex=1) 
 
