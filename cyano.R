13db6c5adc737aff75b184ed13f9c4030dcdf2d2
kiristern <kiri.a.stern@gmail.com>
2020-04-07 11:15:33 -0400

    clean cyano data

---
+++ cyano.R
@@ -0,0 +1,21 @@
+setwd("~/Documents/GitHub/PBIN/data/cyano")
+
+cyano_counts <- read.table("Champ_ASVs_counts.txt", header = TRUE, row.names = 1)
+cyano_taxa <- read.table("Champ_ASVs_taxonomy.txt", sep = "", fill = TRUE)
+cyano <- read.table("Champlain_cyano.txt", fill = TRUE)
+vir_data <- abundance_removed
+
+#extract viral data sample dates
+row.names(vir_data) <- sub("*._*._*._*._*._*._*._","", row.names(vir_data))
+#change "_" to "."
+row.names(vir_data) <- gsub("_", ".", row.names(vir_data))
+#remove everything after last "."
+row.names(vir_data)<-sub(".[^.]+$", "", row.names(vir_data))
+
+#remove X at beginning of date
+colnames(cyano_counts)[1:135] <- substring(colnames(cyano_counts)[1:135], 2)
+#remove everything after last period in colnames
+colnames(cyano_counts)[c(1, 5:63, 65:135)] <- sub(".[^.]+$", "", colnames(cyano_counts)[c(1, 5:63, 65:135)])
+
+#compare samples in common between vir and cyano
+row.names(vir_data) %in% colnames(cyano_counts)
