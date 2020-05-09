34e4aef99a52ee10b38ba795e4cc002f38a0cf98
kiristern <kiri.a.stern@gmail.com>
2020-05-09 13:05:05 -0400

    add linear reg

--- Analysis.R
+++ Analysis.R
@@ -1,9 +1,10 @@
 setwd("~/Documents/GitHub/PBIN/data")
 
 #####Abundance#####
-df <- read.csv("FINAL_ASV_TAXA_ABUN.csv")
-
+# df <- read.csv("FINAL_ASV_TAXA_ABUN.csv")
 #TODO: make sure col name is "abundance"
+
+# TODO: RUN ABUNDANCE_CLEANING.R SCRIPT
 df %>% group_by(samples, ASV_ID) %>% summarise(Relative_Abundance = sum(abundance)) %>%
   ggplot(aes(x = samples, y = Relative_Abundance, fill = ASV_ID)) + 
   geom_bar(stat = "identity", width=1, show.legend = F) +
@@ -48,6 +49,31 @@ observed <- sample_richness(viral_physeq)
 summary(observed)
 plot(observed, viral_physeq, color="Years")
 
+#get linear regression
+alpha_df <- estimate_richness(viral_physeq)
+alpha_df <- as.data.frame(alpha_df$Observed)
+alpha_df <- tibble::rowid_to_column(alpha_df, "Sample")
+names(alpha_df)[2] <- "richness"
+plot(alpha_df)
+abline(lm(richness ~ Sample, data=alpha_df))
+#get adjusted R2
+alphadf_summary <- summary(lm(richness~Sample, data = alpha_df))
+alpha_r2 <- alphadf_summary$adj.r.squared
+#get p-value
+alpha_p <- alphadf_summary$coefficients[2,4]
+#add to graph
+rp = vector('expression',2)
+rp[1] = substitute(expression(italic(R)^2 == MYVALUE), 
+                   list(MYVALUE = format(alpha_r2,dig=3)))[2]
+rp[2] = substitute(expression(italic(p) == MYOTHERVALUE), 
+                   list(MYOTHERVALUE = format(alpha_p, digits = 2)))[2]
+legend("topleft", legend = rp, bty = "n")
+
+#find linear regression formula
+lm(richness ~ Idx, data = alpha_df)
+#y = 0.9103x + 188.7873
+
+
 #observed richness 
 #Sequencing depth by Year
 # data.frame("observed_richness" = (observed %>% summary)$estimate,
@@ -73,8 +99,8 @@ plot(observed, viral_physeq, color="Years")
 #estimate number of missing species using a species richness estimate
 ba <- breakaway(viral_physeq)
 ba
-plot(ba, viral_physeq, color="Years")
-abline(lm(viral_physeq))
+plot(ba, viral_physeq, color="Years") 
+#y = 0.9103x + 188.7873
 
 #get sample names
 # x <- viral_physeq %>% sample_data %>% rownames()
@@ -98,11 +124,24 @@ abline(lm(viral_physeq))
 # arrows(x, richness-std_dev, x, richness+std_dev, length=0.05, angle = 90, code=3)
 
 #for variables Years and bloom2
-ba_alpha = data.frame("ba_observed_richness" = (ba %>% summary)$estimate,
-           "Bloom" = viral_physeq %>% sample_data %>% get_variable("bloom2"))
+ba_years = data.frame("ba_observed_richness" = (ba %>% summary)$estimate,
+           "Years" = viral_physeq %>% sample_data %>% get_variable("Years"))
+#write.table(ba_years, "ba_years.txt")
+ba_years <- read.table("ba_years.txt", header = T)
+fit <- lm(ba_observed_richness ~ Years, data = ba_years)
+coefs <- coef(fit)
+#get r2
+(r2 = signif(summary(fit)$adj.r.squared))
+#get p-value
+(pval <- signif(summary(fit)$coef[2,4]))
 
-(ba_plot <-  ggplot(ba_alpha, aes(x = Bloom, y = ba_observed_richness))+
-  geom_point())
+(ba_plot <-  ggplot(ba_years, aes(x = Years, y = ba_observed_richness))+
+  geom_point()+
+    geom_abline(intercept = coefs[1], slope = coefs[2])+
+    labs(title = paste("Adj R2 = ", r2,
+                       #"Intercept =", signif(coefs[1]),
+                       #"Slope =", signif(coefs[2]),
+                       "p-value =", pval)))
 
 #geom_crossbar()
 ba_plot + stat_summary(fun.data="mean_sdl", fun.args = list(mult=1), 
@@ -162,21 +201,58 @@ dv_viral_ps <- divnet(viral_physeq, ncores = 4)
 
 
 ######## Beta Diversity ##########
+#subset data between bloom and non bloom samples
+data_bloom <- subset_samples(viral_physeq, bloom2=="yes")
+data_no_bloom <- subset_samples(viral_physeq, bloom2=="no")
+
+
 # PCoA plot using the unweighted UniFrac and JSD as distance
 unifrac_dist <- phyloseq::distance(viral_physeq, method="unifrac", weighted=T)
 jsd_dist <- sqrt(phyloseq::distance(viral_physeq, "jsd")) #jsd is a semi-metric
+#PCoA bloom
+unifrac_dist_bloom <- phyloseq::distance(data_bloom, method="unifrac", weighted=T)
+jsd_dist_bloom <- sqrt(phyloseq::distance(data_bloom, "jsd")) #jsd is a semi-metric
+#PCoA no bloom
+unifrac_dist_nob <- phyloseq::distance(data_no_bloom, method="unifrac", weighted=T)
+jsd_dist_nob <- sqrt(phyloseq::distance(data_no_bloom, "jsd")) #jsd is a semi-metric
+
 
 #change distance=unifrac_dist to jsd_dist
 ordination <- ordinate(viral_physeq, method="PCoA", distance=jsd_dist)
 plot_ordination(viral_physeq, ordination, color="Years") + 
-  #stat_ellipse(type = "norm", linetype = 2) +
-  stat_ellipse(type = "t") +
+  #stat_ellipse(type = "norm")+#, linetype = 2) +
+  #stat_ellipse(type = "t") +
   theme(aspect.ratio=1) + 
   theme_classic() #+
-  #theme_bw()
+#theme_bw()
+#plot PCoA bloom
+ordination_bloom <- ordinate(data_bloom, method="PCoA", distance=jsd_dist_bloom)
+plot_ordination(data_bloom, ordination, color="Years") + 
+  #stat_ellipse(type = "norm")+#, linetype = 2) +
+  #stat_ellipse(type = "t") +
+  theme(aspect.ratio=1) + 
+  theme_classic() #+
+   #theme_bw()
+#Plot PCoA no bloom
+ordination_nob <- ordinate(data_no_bloom, method="PCoA", distance=jsd_dist_nob)
+plot_ordination(data_no_bloom, ordination, color="Years") + 
+  #stat_ellipse(type = "norm")+#, linetype = 2) +
+  #stat_ellipse(type = "t") +
+  theme(aspect.ratio=1) + 
+  theme_classic() #+
+#theme_bw()
+
+
 
 #Test whether the Years/Months differ significantly from each other using the permutational ANOVA (PERMANOVA) analysis:
 adonis(unifrac_dist ~ sample_data(viral_physeq)$Years)
 adonis(unifrac_dist ~ sample_data(viral_physeq)$Months)
+
+#Permanova test using adonis function
+adonis(dist ~ variable, as(sample_data(data), "data.frame"))
+
+
+
+
 
 
