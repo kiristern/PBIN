ee3ad963404c6d9be84d0ac9766c6d20b4d9dfa2
kiristern <kiri.a.stern@gmail.com>
2021-01-28 22:00:21 -0500

    plotting connectivity figures! yay!

--- spieceasi.R
+++ spieceasi.R
@@ -18,6 +18,8 @@ sample_info <- sample_data(meta2)
 
 #add to phyloseq object
 virps <- phyloseq(count_phy, sample_info, mock_taxa2)
+colnames(tax_table(virps)) <- c("Kingdom", "Phylum", "Class",
+                                      "Order", "Family", "Genus", "ASV")
 
 #reorder phyloseq by chronological date
 otu_table(virps) <- otu_table(virps)[,toorder]
@@ -32,15 +34,12 @@ spie <- spiec.easi(list(virps_filt, cyanops_filt), method='mb', nlambda=40,
                       lambda.min.ratio=1e-2, pulsar.params = list(thresh = 0.05))
 
 dtype <- c(rep(1,ntaxa(virps_filt)), rep(2,ntaxa(cyanops_filt)))
-plot(adj2igraph(getRefit(spie)), vertex.color=dtype+1, vertex.size=9)
-
+#plot(adj2igraph(getRefit(spie)), vertex.color=dtype+1, vertex.size=9)
 
 
 #http://psbweb05.psb.ugent.be/conet/microbialnetworks/spieceasi.php
 betaMatsym <- as.matrix(symBeta(getOptBeta(spie)))
 dim(betaMatsym)
-betaMat <- getOptBeta(spie)
-dim(betaMat)
 
 #check positive, negative, and total edges
 (p.edge =length(betaMat[betaMatsym>0])/2)
@@ -63,10 +62,58 @@ plot_network(FG.ig, list(virps_filt, cyanops_filt))
 #postive weights only:
 weights.pos <- (1-Matrix::summary(t(bm))[,3])/2
 FG.ig.pos <- adj2igraph(Matrix::drop0(getRefit(spie)),
-                    edge.attr=list(weight=weights.pos),
-                    vertex.attr = list(name=c(taxa_names(virps_filt), taxa_names(cyanops_filt))))
+                    edge.attr=list(weight=weights.pos))
 plot_network(FG.ig.pos, list(virps_filt, cyanops_filt))
 
 write.graph(FG.ig,"spieceasi.ncol.txt",format="ncol") 
-head(spea2cytoscape <- read.table("spieceasi.ncol.txt"))
+head(corr.tab <- read.table("spieceasi.ncol.txt"))
+
+#Create a custom color scale
+library(RColorBrewer)
+dtype <- as.factor(c(rep(1,ntaxa(virps_filt)), rep(2,ntaxa(cyanops_filt))))
+otu.id <- c(taxa_names(virps_filt), taxa_names(cyanops_filt))
+colours.df <- cbind(data.frame(otu.id), data.frame(dtype))
+
+myColors <- brewer.pal(2,"Set1")
+names(myColors) <- levels(colours.df$dtype)
+nodeColr <- scale_colour_manual(name = "dtype",values = myColors)
+
+
+
+#https://ramellose.github.io/networktutorials/workshop_MDA.html
+library(igraph)
+#Network centrality: degree centrality (ie. degree = number of connections a node has)
+spiec.deg <- degree(FG.ig)
+hist(spiec.deg)
+
+#if the degree distribution of a network follows a power law, that network is scale-free
+plaw.fit <- fit_power_law(spiec.deg) #The fit_power_law functions fits a power law to the degree distribution of the network.
+plaw.fit
+#The values for the fit are compared to expected values with the Kolmogorov-Smirnov test. 
+#The null hypothesis for this test is that the degree distribution is drawn from a reference distribution. 
+#In this case, that reference distribution is generated from a power law.
+
+#The null hypothesis can only be rejected if the p-value of the test is below 0.05. 
+#Here, the p-value is 1. Therefore, we cannot conclude that the degree distribution is drawn from a different distribution than the power-law distribution.
+#Scale-free networks are networks with a degree distribution that follows a power law. 
+#Our result indicates that the network may be scale-free and contains nodes with a degree far larger than the average degree. 
+#While there is not that much known about the effect of scale-freeness on microbial networks, studies (e.g., Cohen et al 2001, https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.86.3682) 
+#indicate that scale-freeness decreases the network’s sensitivity to random attacks. 
+#However, we still do not know to what extent biological networks follow a power law as we have few true biological networks.
+#Lima-Mendez and van Helden (2009) (https://pubs.rsc.org/en/content/articlehtml/2009/mb/b908681a) discuss some of the weaknesses of this theory.
+
+#plot degree as node size in the network
+plot_network(FG.ig, 
+             list(virps_filt, cyanops_filt),
+             type="taxa")+
+  geom_point(aes(size=spiec.deg,
+                 fill = factor(colours.df$dtype)),
+             colour='deepskyblue4',
+             shape=21)+
+  scale_fill_manual(values=c("forestgreen", "cyan4"))+
+  labs(fill="Type", size="Degree of centrality")+
+  scale_fill_discrete(name="Type", labels=c("Viral", "Bacterial"))
+
+
+
 
