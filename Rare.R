321cceba1d746e675f540dd5c8bc83adb6e6c38b
kiristern <kiri.a.stern@gmail.com>
2021-02-13 12:20:01 -0500

    create phyloseq object with cond rare

--- Rare.R
+++ Rare.R
@@ -184,7 +184,7 @@ SimpleRareToPrev.f=function(otu_fp,abund_thresh = 0.005, abund_thresh_ALL=FALSE,
 (cond_rare <- SimpleRareToPrev.f(otu_fp="/Users/kiristern/Documents/GitHub/PBIN/data/ASVs_counts_copy.tsv",abund_thresh=0.005, abund_thresh_ALL=FALSE,b_thresh=0.90, rdp_lastcol=FALSE))
 
 #select all the conditionally rare ASVs from phyloseq object
-crare_virps <- otu_table(viral_physeq)[row.names(viral_physeq %>% otu_table()) %in% cond_rare$OTUID, ]
+crare_virps <- subset_taxa(viral_physeq, (rownames(tax_table(viral_physeq)) %in% cond_rare$OTUID))
 tcrare_virps <- t(crare_virps)
 
 #store data in timeseries object
