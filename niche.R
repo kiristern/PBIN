6a5402583d5430dd953ee5709fae60c30b60dba8
kiristern <kiri.a.stern@gmail.com>
2021-02-28 19:59:56 -0500

    plot gen.dist vs covar/corr

--- niche.R
+++ niche.R
@@ -1,9 +1,28 @@
-####### clusters ASVs to form new OTUs #######
 #https://github.com/benjjneb/dada2/issues/947
 #https://github.com/adriaaulaICM/bbmo_niche_sea/blob/master/src/analysis/nucdist_otuclustering.R
 library(tibble)
 library(DECIPHER)
 library(Biostrings)
+library(reshape2)
+
+#sparcc values
+options(stringsAsFactors=FALSE)
+.Options$sig_pval <- 0.05
+# Read in data
+d.corr_full <- read.table('median_correlation.tsv', sep='\t', header=TRUE, comment.char='', row.names=1, check.names=FALSE)
+d.pval_full <- read.table('pvalues.tsv', sep='\t', header=TRUE, comment.char='', row.names=1, check.names=FALSE)
+# Mask upper triangle with NAs then melt, excluding upper triangle values
+d.corr_full[upper.tri(d.corr_full, diag=TRUE)] <- NA
+d.pval_full[upper.tri(d.pval_full, diag=TRUE)] <- NA
+d.corr <- melt(as.matrix(d.corr_full), na.rm=TRUE, varnames=c('otu_1', 'otu_2'), value.name='correlation')
+d.pval <- melt(as.matrix(d.pval_full), na.rm=TRUE, varnames=c('otu_1', 'otu_2'), value.name='pvalue')
+# Merge correlations and pvalues, apply multiple testing correction
+sparc <- merge(d.corr, d.pval)
+sparc$pvalue_adjusted <- p.adjust(sparc$pvalue, method='BH')
+# Print rows with a significant pvalue, after multiple testing correction
+sparc <- print(sparc[sparc$pvalue_adjusted <= .Options$sig_pval, ], row.names=FALSE)
+
+
 
 # BiocManager::install("DECIPHER")
 # library("DECIPHER")
@@ -21,6 +40,7 @@ d.align[1:5]
 # names(gendist)[names(gendist) == "key"] <- "from"
 # dim(gendist)
 dim(vir.corr.tab)
+names(vir.corr.tab)[names(vir.corr.tab) == "weight"] <- "covariance"
 # dim(gendist.table)
 
 ind <- which(upper.tri(d.align, diag = F), arr.ind = TRUE)
@@ -33,13 +53,128 @@ dim(gendist.tab)
 df3 <- inner_join(gendist.tab, vir.corr.tab, by = c("to", "from"))
 df4 <- inner_join(gendist.tab, vir.corr.tab, by = c("to" = "from", "from" = "to"))
 gendist.covar <- rbind(df3,df4) %>% unique()
+#write.csv(gendist.covar, "gendist.covar.csv")
+head(gendist.covar <- read.csv("gendist.covar.csv", header = T, row.names = 1))
+
+df5 <- inner_join(gendist.covar,sparc, by = c("to" = "otu_1", "from" = "otu_2"))
+df6 <- inner_join(gendist.covar, sparc, by = c("from" = "otu_1", "to" = "otu_2"))
+gendist.sparcc <- rbind(df5, df6) %>% unique()
+#write.csv(gendist.sparcc, "gendist.sparcc.csv")
+head(gendist.sparcc <- read.csv("gendist.sparcc.csv", header = T, row.names = 1))
+
+
 dim(gendist.covar)
+dim(gendist.sparcc)
+head(gendist.covar)
+head(gendist.sparcc)
 #write.csv(gendist.covar, "gendist.covar.csv")
 
 
+### plot boxplots ###
+
+fit <- lm(covariance ~ gen.dist , data = gendist.covar)
+(coefs <- coef(fit))
+#get r2
+(r2 = signif(summary(fit)$adj.r.squared))
+#get p-value
+(pval <- signif(summary(fit)$coefficients[2,4]))
+ggplot(gendist.covar, aes(x=gen.dist, y=covariance, group=gen.dist))+
+  geom_boxplot()+
+  geom_jitter()+
+  geom_smooth(method = "lm", se=F, color="red", aes(group=1))+
+  labs(title = paste("Adj R2 = ", r2,
+                     #"Intercept =", signif(coefs[1]),
+                     "Slope =", signif(coefs[2]),
+                     "p-value =", pval))
+  #+ coord_cartesian(xlim = c(0.0, 0.5))
+
+
+#### filter limits ####
+gc.filt <- gendist.covar %>%
+  filter(gen.dist > 0 & gen.dist <0.05)
+dim(gc.filt)
+
+fit <- lm(covariance ~ gen.dist , data = gc.filt)
+coefs <- coef(fit)
+summary(fit)
+#get r2
+(r2 = signif(summary(fit)$adj.r.squared))
+#get p-value
+(pval <- signif(summary(fit)$coefficients[2,4]))
+ggplot(gc.filt, aes(x=gen.dist, y=covariance, group=gen.dist))+
+  geom_boxplot()+
+  geom_jitter()+
+  geom_smooth(method = "lm", se=F, color="red", aes(group=1))+
+  labs(title = paste("Adj R2 = ", r2,
+                     #"Intercept =", signif(coefs[1]),
+                     "Slope =", signif(coefs[2]),
+                     "p-value =", pval))
+
+
+fit <- lm(correlation ~ gen.dist , data = gendist.sparcc)
+(coefs <- coef(fit))
+#get r2
+(r2 = signif(summary(fit)$adj.r.squared))
+#get p-value
+(pval <- signif(summary(fit)$coefficients[2,4]))
+ggplot(gendist.sparcc, aes(x=gen.dist, y=correlation, group=gen.dist))+
+  geom_point()+
+  geom_boxplot()+
+  geom_smooth(method = "lm", se=F, color="red", aes(group=1))+
+  labs(title = paste("Adj R2 = ", r2,
+                     #"Intercept =", signif(coefs[1]),
+                     "Slope =", signif(coefs[2]),
+                     "p-value =", pval))
 
 
 
+gs.filt.pos <- gendist.sparcc %>%
+  filter(correlation > 0 & gen.dist <0.05)
+
+fit <- lm(correlation ~ gen.dist , data = gs.filt.pos)
+(coefs <- coef(fit))
+#get r2
+(r2 = signif(summary(fit)$adj.r.squared))
+#get p-value
+(pval <- signif(summary(fit)$coefficients[2,4]))
+ggplot(gs.filt.pos, aes(x=gen.dist, y=correlation, group=gen.dist))+
+  geom_point()+
+  geom_boxplot()+
+  geom_jitter()+
+  geom_smooth(method = "lm", se=F, color="red", aes(group=1))+
+  labs(title = paste("Adj R2 = ", r2,
+                     #"Intercept =", signif(coefs[1]),
+                     "Slope =", signif(coefs[2]),
+                     "p-value =", pval))+
+  coord_cartesian(xlim = c(0.0, 0.05)) #zoom in over these coords only without rm other data points 
+
+
+
+gs.filt.neg <- gendist.sparcc %>%
+  filter(correlation < 0 )
+
+fit <- lm(correlation ~ gen.dist , data = gs.filt.neg)
+(coefs <- coef(fit))
+#get r2
+(r2 = signif(summary(fit)$adj.r.squared))
+#get p-value
+(pval <- signif(summary(fit)$coefficients[2,4]))
+ggplot(gs.filt.neg, aes(x=gen.dist, y=correlation, group=gen.dist))+
+  geom_point()+
+  geom_boxplot()+
+  geom_jitter()+
+  geom_smooth(method = "lm", se=F, color="red", aes(group=1))+
+  labs(title = paste("Adj R2 = ", r2,
+                     #"Intercept =", signif(coefs[1]),
+                     "Slope =", signif(coefs[2]),
+                     "p-value =", pval))
+  #coord_cartesian(xlim = c(0.0, 0.5))
+
+
+
+####### clusters ASVs to form new OTUs #######
+#https://github.com/adriaaulaICM/bbmo_niche_sea/blob/master/src/analysis/nucdist_otuclustering.R
+#https://github.com/benjjneb/dada2/issues/947 
 cluster99 <- DECIPHER::IdClusters(
   d.align,
   method = "complete",
